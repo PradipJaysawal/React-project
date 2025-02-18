5ff287df6e686e1c27533e3dfce3bfed7ef736bc
PradipJaysawal <gabbarsamba948@gmail.com>
2025-02-18 21:16:43 +0545

    time 06/11

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,15 +3,13 @@ function Navbar(){
     return(
         <>
         <div className="fixed top-0 right-0 w-full bg-blue-200 shawod-md">
-            <div className="container bg-yellow-100 ">
                 <div className="flex justify-between px-15">
                     <p>081/06/11, 8:30 PM</p>
                     <p>98********</p>
                 </div>
-            </div>
         </div>
-        
-        <div>
+
+         <div>
         <nav className="flex justify-between items-center px-15 py-2 bg-gray-100 mt-6">
             <img src={owlbird} alt="logo" className="h-20"/>
             <ul className="flex justify-center space-x-4">
@@ -21,7 +19,7 @@ function Navbar(){
                 <li><a href="#" className="text-blue-500">Contact</a></li>
             </ul>
         </nav>
-        </div>
+        </div> 
         </>
     )
 }
