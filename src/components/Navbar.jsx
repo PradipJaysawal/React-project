75d958d133f57c4b241cf727865f40f1289692d6
PradipJaysawal <gabbarsamba948@gmail.com>
2025-02-18 07:22:06 +0545

    logo change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
-import smclogo from'../assets/smclogo.png';
+import owlbird from'../assets/owlbird.png';
 function Navbar(){
     return(
         <div>
         <nav className="flex justify-between items-center px-15 py-2 bg-gray-100">
-            <img src={smclogo} alt="logo" className="h-20"/>
+            <img src={owlbird} alt="logo" className="h-20"/>
             <ul className="flex justify-center space-x-4">
                 <li><a href="#" className="text-blue-500">Home</a></li>
                 <li><a href="#" className="text-blue-500">About</a></li>
