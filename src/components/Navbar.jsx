34fc4e19e5ae1453ec90d0d4434a77d796e0e6a3
PradipJaysawal <gabbarsamba948@gmail.com>
2025-02-18 20:46:50 +0545

    time & contact 06/11

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,18 @@
 import owlbird from'../assets/owlbird.png';
 function Navbar(){
     return(
+        <>
+        <div className="fixed top-0 right-0 w-full bg-blue-200 shawod-md">
+            <div className="container bg-yellow-100 ">
+                <div className="flex justify-between px-15">
+                    <p>081/06/11, 8:30 PM</p>
+                    <p>98********</p>
+                </div>
+            </div>
+        </div>
+        
         <div>
-        <nav className="flex justify-between items-center px-15 py-2 bg-gray-100">
+        <nav className="flex justify-between items-center px-15 py-2 bg-gray-100 mt-6">
             <img src={owlbird} alt="logo" className="h-20"/>
             <ul className="flex justify-center space-x-4">
                 <li><a href="#" className="text-blue-500">Home</a></li>
@@ -12,6 +22,7 @@ function Navbar(){
             </ul>
         </nav>
         </div>
+        </>
     )
 }
 export default Navbar;
