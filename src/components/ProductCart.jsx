3f28baa3141dbc042554ab5c8edb917493573140
PradipJaysawal <gabbarsamba948@gmail.com>
2025-02-20 10:12:50 +0545

    08/11

--- src/components/ProductCart.jsx
+++ src/components/ProductCart.jsx
@@ -7,7 +7,7 @@ function ProductCart(){
         <img src="https://images.unsplash.com/photo-1739932215472-15ebf0ab6cf4?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="product" className="h-40 w-full object-cover" />
         <h2 className="text-lg fontsemibold mt4-">Product Name</h2>
         <h2 className="text-lg fontsemibold mt4-">Product Description</h2>
-        <h2 className="text-lg fontsemibold mt4-">$100</h2>
+        <h2 className="text-lg fontsemibold mt4-">$200</h2>
         </a>
         </div>
     )
