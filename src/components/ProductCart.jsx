b1480a031742c20943ca015d9155c2fea561119d
PradipJaysawal <gabbarsamba948@gmail.com>
2025-02-23 06:58:32 +0545

    11/11

--- src/components/ProductCart.jsx
+++ src/components/ProductCart.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-function ProductCart(){
+function ProductCart({product}){
     return(
         <div>
         <a href="" className="bg-white p-4 rounded-lg shadow-lg block hover:shadow-xl hover:-translate-y-1 transition duration-200">
-        <img src="https://images.unsplash.com/photo-1739932215472-15ebf0ab6cf4?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="product" className="h-40 w-full object-cover" />
-        <h2 className="text-lg fontsemibold mt4-">Product Name</h2>
-        <h2 className="text-lg fontsemibold mt4-">Product Description</h2>
-        <h2 className="text-lg fontsemibold mt4-">$200</h2>
+        <img src={product.image }alt="product" className="h-40 w-full object-cover" />
+        <h2 className="text-lg fontsemibold mt4-">{product.productname}</h2>
+        <h2 className="text-lg fontsemibold mt4-">{product.description}</h2>
+        <h2 className="text-lg fontsemibold mt4-">${product.price}</h2>
         </a>
         </div>
     )
--- src/components/ProductContainer.jsx
+++ src/components/ProductContainer.jsx
@@ -2,13 +2,46 @@ import React from "react";
 import ProductCart from "./ProductCart";
 
 function ProducutContainer(){
+    const products = [
+        {
+            id: 1,
+            productname: "product 1",
+            description: "this is description",
+            price:100,
+            image:'https://picsum.photos/200'
+        },
+
+        {
+            id: 2,
+            productname: "product 2",
+            description: "This is about id:2",
+            price:200,
+            image:'https://picsum.photos/300'
+        },
+
+        {
+            id: 3,
+            productname: "product 3",
+            description: "This is about id:3",
+            price:300,
+            image:'https://picsum.photos/400'
+        },
+
+        {
+            id: 4,
+            productname: "product 4",
+            description: "This is about id:4",
+            price:400,
+            image:'https://picsum.photos/600'
+        },
+    ]
     return(
         <div>
             <div className="grid grid-cols-4 gap-4 px-20 py-20">
-                <ProductCart />
-                <ProductCart />
-                <ProductCart />
-                <ProductCart />
+                {products.map((product)=>(
+                    // <ProductCart key={product.id} productname={product.productname} price={product.price} description={product.description} /> //single product
+                    <ProductCart key={product.id} product={product} /> 
+                ))}
             </div>
         </div>
     )
