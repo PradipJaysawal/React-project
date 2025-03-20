41d72afa01af5614624c7052b4e2a79b651f5867
PradipJaysawal <gabbarsamba948@gmail.com>
2025-03-20 06:57:50 +0545

    modify

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -5,9 +5,9 @@ function ServiceCard({service}){
         <>
         <div>
             <a href="" className="bg-gray-200 p-4 rounded-lg shadow-lg block hover:shadow-xl hover:-translate-y-1 transition duration-200">
-            {/* <img src={service.image} alt="product" className="h-40 w-full object-cover" /> */}
-                <h2 className="text-lg font-semibold mt4-">{service.title}</h2>
-                <h2 className="text-lg mt4-">{service.description}</h2>
+             <img src={service.image} alt="product" className="h-40 w-full object-cover" /> 
+                <h2 className="text-lg font-semibold mt-4">{service.title}</h2>
+                <h2 className="text-lg mt-4 line-clamp-2">{service.description}</h2>
                 </a>
         </div>
         </>
