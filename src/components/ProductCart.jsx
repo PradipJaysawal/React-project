19e42221d5fe002353803dd81c976708c6e36eae
PradipJaysawal <gabbarsamba948@gmail.com>
2025-04-08 19:46:43 +0545

    update

--- src/components/ProductCart.jsx
+++ src/components/ProductCart.jsx
@@ -6,8 +6,8 @@ function ProductCart({product,onClick}){
         <div onClick={onClick}>
         <a href="" className="bg-white p-4 rounded-lg shadow-lg block hover:shadow-xl hover:-translate-y-1 transition duration-200">
         <img src={API_URL_PRODUCT+product.photopath} alt="product" className="h-64 w-full object-cover" />
-        <h2 className="text-lg font-semibold mt-4">{product.name}</h2>
-        <h2 className="text-lg mt-4 line-clamp-2">{product.description}</h2>
+        <h2 className="text-lg font-semibold mt-4  line-clamp-1">{product.name}</h2>
+        <h2 className="text-lg mt-4 line-clamp-1">{product.description}</h2>
         <h2 className="text-lg font-semibold mt-4">${product.price}</h2>
         
         </a>
